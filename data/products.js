899f01c5bcdd68a2f0c01b87c809636ccf2bbaf8
s13bp <86206902+s13bp@users.noreply.github.com>
2023-02-25 22:51:14 +0300

    hh

---
+++ data/products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    name: 'Ledger Nano X',
+    image: '/images/ledger.jpeg',
+    description:
+      'Be safe with your crpto',
+    brand: 'Satoshi',
+    category: 'Electronics',
+    price: 180.99,
+    countInStock: 13,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'MegaMeta Ebook',
+    image: '/images/book.jpeg',
+    description:
+      'Set yourself free financially my dude',
+    brand: 'Apple',
+    category: 'Books',
+    price: 9.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+
+]
+
+export default products
